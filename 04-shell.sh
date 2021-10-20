07a7114d3d8e6ce98aac1ab3f2343a97f798e278
lavanya <lavanya.bhadri>
2021-10-20 16:36:40 +0530

    colouring the content with all colors

--- 04-shell.sh
+++ 04-shell.sh
@@ -2,4 +2,9 @@
 
 echo -e "hello\tworld\thai\nhow\tare\tyou"
 
-echo -e "\e[31mtext in green colour"
+echo -e "\e[31mtext in red colour"
+echo -e "\e[32mtext in green colour"
+echo -e "\e[33mtext in yellow colour"
+echo -e "\e[34mtext in blue colour"
+echo -e "\e[35mtext in meganta colour"
+echo -e "\e[36mtext in cyan colour"
