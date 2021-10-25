ec0a4af83ad72a97eba0046f2842fdebdad23672
lavanya <lavanya.bhadri@gmail.com>
2021-10-25 23:14:28 +0530

    writing function script

--- 01-function.sh
+++ 01-function.sh
@@ -4,11 +4,18 @@ function abc(){
   echo i am a function abc
   echo a in function= $a
   b=20
+  return 20
+  echo first argument in function= $1
+  }
+xyz() {
+  echo i am a function xyz
 }
 a=10
-abc
+abc $1
+echo exit status of abc = $?
 echo b in main programe=$b
-
+xyz
+echo first argument in main program = $1
 
 
 
