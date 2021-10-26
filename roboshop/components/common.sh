d291e72e8e17ad4d13c8ceb88788c2cb38e104c2
lavanya <lavanya.bhadri@gmail.com>
2021-10-26 21:19:52 +0530

    calling common file

---
+++ roboshop/components/common.sh
@@ -0,0 +1,15 @@
+print(){
+  echo -n -e "\e[1m$1\e[0m........"
+  echo -e "\n\e[36m#######################$1#######################\e[0m" >>$LOG
+}
+stat(){
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[1;32msuccess\e[0m"
+  else
+    echo -e "\e[1;31mfailure\e[0m"
+    echo -e "\e[1;36mscript failed and check the detailed log in $LOG file"
+    exit 1
+  fi
+}
+LOG=/tmp/roboshop.log
+rm -f $LOG
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-print(){
-  echo -n -e "\e[1m$1\e[0m........"
-  echo -e "\n\e[36m#######################$1#######################\e[0m" >>$LOG
-}
-stat(){
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[1;32msuccess\e[0m"
-  else
-    echo -e "\e[1;31mfailure\e[0m"
-    echo -e "\e[1;36mscript failed and check the detailed log in $LOG file"
-    exit 1
-  fi
-}
-LOG=/tmp/roboshop.log
-rm -f $LOG
+source components/common.sh
 print "Installing Nginx"
 yum install nginxxx -y &>>$LOG
 stat $?
