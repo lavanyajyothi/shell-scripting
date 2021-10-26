538fe9f5e66951248fa71167db3f31ce92ec7e59
lavanya <lavanya.bhadri@gmail.com>
2021-10-26 16:05:15 +0530

    enabling nginx

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo -e "\e[1mInstalling Nginx\e[0m"
+print(){
+  echo -e "\e[1m$1\e[0m"
+}
+print "Installing Nginx"
 yum install nginx -y
 
-echo -e "\e[1mEnabling Nginx\e[0m"
+print "Enabling Nginx"
 systemctl enable nginx
-echo -e "\e[1mStarting Nginx\e[0m"
+print "starting Nginx"
 systemctl start nginx
 
 exit
