7d274cfbcda9d891b0f4cbf9de0f061ebfc54b89
lavanya <lavanya.bhadri@gmail.com>
2021-10-27 22:51:56 +0530

    script of catalogue

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -6,24 +6,38 @@ yum install nodejs make gcc-c++ -y &>>$LOG
 stat $?
 
 print "Add Roboshop User"
-useradd roboshop &>>$LOG
+id roboshop &>>$LOG
+if [$? -eq 0 ]; then
+  echo user roboshop already exist &>>$LOG
+else
+  useradd roboshop &>>$LOG
+fi
 stat $?
 
 print "Download Catalogue"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
 stat $?
 
-print "Old Content"
+print "Remove Old Content"
 rm -rf /home/roboshop/catalogue
 stat $?
 
 print "Extract Catalogue"
-$ unzip -o -d /home/roboshop /tmp/catalogue.zip
+unzip -o -d /home/roboshop /tmp/catalogue.zip
 stat $?
-#$ mv catalogue-main catalogue
-#$ cd /home/roboshop/catalogue
-#$ npm install
 
+print "Copy Content"
+mv /home/roboshopcatalogue-main /home/roboshop/catalogue
+stat $?
+
+print "Install NodeJS Dependencies"
+cd /home/roboshop/catalogue
+npm install --unsafe-perm &>>$LOG
+stat $?
+
+print "Fix App Permissions"
+chown -R roboshop:roboshop /home/roboshop
+stat $?
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
