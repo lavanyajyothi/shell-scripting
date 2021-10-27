8482b2488f5ff802a738dbc534d46e711a9d9deb
lavanya <lavanya.bhadri@gmail.com>
2021-10-27 14:36:05 +0530

    download html pages

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -11,16 +11,15 @@ print "starting Nginx"
 systemctl start nginx
 stat $?
 
-exit
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-# Deploy in Nginx Default Location.
-
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-master static README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+print "download Html pages"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+stat $?
+#cd /usr/share/nginx/html
+#rm -rf *
+#unzip /tmp/frontend.zip
+#mv frontend-main/* .
+#mv static/* .
+#rm -rf frontend-master static README.md
+#mv localhost.conf /etc/nginx/default.d/roboshop.conf
+#systemctl restart nginx
 
