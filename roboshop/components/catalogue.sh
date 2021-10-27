b08757ae1aa4af7860cf2bdb33316b440259dc31
lavanya <lavanya.bhadri@gmail.com>
2021-10-27 22:55:07 +0530

    script of catalogue

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -27,7 +27,7 @@ unzip -o -d /home/roboshop /tmp/catalogue.zip
 stat $?
 
 print "Copy Content"
-mv /home/roboshopcatalogue-main /home/roboshop/catalogue
+mv /home/roboshop/catalogue-main /home/roboshop/catalogue
 stat $?
 
 print "Install NodeJS Dependencies"
