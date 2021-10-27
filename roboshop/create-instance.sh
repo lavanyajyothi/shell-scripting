5d2733faa7ef7be9f9846d01720b736bb8ef1869
lavanya <lavanya.bhadri@gmail.com>
2021-10-27 17:10:19 +0530

    tag instances

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=$1}]"
+aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"
 
