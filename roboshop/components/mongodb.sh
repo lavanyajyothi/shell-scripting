7a94d267f11c79c1c8481e02a7f8622a5f7d8859
lavanya <lavanya.bhadri@gmail.com>
2021-10-27 22:10:39 +0530

    script of mongodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -3,19 +3,20 @@
 source components/common.sh
 
 print "Download Repo"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
 stat $?
 
 print "install MongoDB"
 yum install -y mongodb-org &>>$LOG
 stat $?
+
 print "Update MongoDB Config"
-sed -i -e's/127.0.0.1/0.0.0.0/g'/etc/mongod.conf &>>$LOG
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG
 stat $?
 
 
 print "start MongoDB"
-systemctl start mongod &>>$LOG
+systemctl restart mongod &>>$LOG
 stat $?
 
 print "enable MongoDB"
@@ -25,6 +26,7 @@ stat $?
 print "Download Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
 stat $?
+
 print "Extract Schema"
 unzip -o -d /tmp /tmp/mongodb.zip &>>$LOG
 stat $?
