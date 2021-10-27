a075da92a980c84d730f88323092231afa66fe55
lavanya <lavanya.bhadri@gmail.com>
2021-10-27 17:32:23 +0530

    conditions applying for telling weather instance is already exist or not if exist it says already present otherwise create one

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" |jq ".Reservations[].Instances[].InstanceId" |wc -l)
+COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" |jq ".Reservations[].Instances[].InstanceId" | wc -l)
 if [$COUNT -eq 0]; then
-  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" |jq
+  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
   echo "Instance already exist"
-
+fi
