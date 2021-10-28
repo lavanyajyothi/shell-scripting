e9468b741aa685dd32d52aa67cf65704c7b24287
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 05:03:01 +0530

    getting DNS name

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
+COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name.Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
 if [ $COUNT -eq 0 ]; then
-  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --security-group-ids sg-0370951ca6dfcb3e7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --security-group-ids sg-0370951ca6dfcb3e7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name.Value=$1}]" | jq
 else
   echo "Instance already exist"
 fi
 
-IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null)
+IP=$(aws ec2 describe-instances --filters "Name=tag:Name.Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null)
 sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zine-id Z08537272MHAQMEL5KTIG --change-batchfile:///tmp/record.json | jq
+aws route53 change-resource-record-sets --hosted-zone-id Z08537272MHAQMEL5KTIG --change-batch file:///tmp/record.json | jq
