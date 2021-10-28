2868cd040b6a07b53f3ab7f9c9da73fac84f2a42
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 04:48:03 +0530

    getting DNS name

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -2,7 +2,7 @@
 
 COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
 if [ $COUNT -eq 0 ]; then
-  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --security-group-ids sg-0370951ca6dfcb3e7 --tag-specifications "ResourceType=instance.Tags=[{Key=Name,Value=$1}]" | jq
+  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --security-group-ids sg-0370951ca6dfcb3e7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
   echo "Instance already exist"
 fi
