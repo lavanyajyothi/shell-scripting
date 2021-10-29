dc62c6f9b62957b51eea9fb06b136cfb515eaf5e
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 16:08:58 +0530

    user file

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -2,7 +2,7 @@
 source components/common.sh
 
 MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
-COMPONENT_NAME = User
+COMPONENT_NAME=User
 COMPONENT=user
 NodeJS
 
