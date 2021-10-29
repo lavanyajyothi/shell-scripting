63c8e7e5c95a5c73e35f397bcb16d504d7377854
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 14:30:55 +0530

    giving spaces to put it in order

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -4,6 +4,7 @@ source components/common.sh
 
 MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
+
 print "install NodeJS"
 yum install nodejs make gcc-c++ -y &>>$LOG
 stat $?
