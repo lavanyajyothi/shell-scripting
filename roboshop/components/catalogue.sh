e7d5244d3a381ea3307e9487984ead4a54e683aa
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 11:35:06 +0530

    giving spaces in catalogue file

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,7 +2,7 @@
 
 source components/common.sh
 
-MLINE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -l)
+MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -l)
 
 
 print "install NodeJS"
