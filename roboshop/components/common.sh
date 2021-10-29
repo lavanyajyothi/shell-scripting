a6f723ea3ba2c703960b4cd2885a63a472cedc0e
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 12:30:18 +0530

    giving spaces

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,7 +1,7 @@
 print(){
   LSPACE=$(echo $1 | awk '{print length}')
-  SPACES=$(($MSPACE - $LSPACE))
-  echo $SPACES
+  SPACES=echo $(($MSPACE - $LSPACE))
+
  echo -n -e "\e[1m$1${SPACES}\e[0m........"
  echo -e "\n\e[36m#######################$1#######################\e[0m" >>$LOG
 }
