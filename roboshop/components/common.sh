dfc442eaf259803db478d3c391501fc65e0d3b5b
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 11:27:48 +0530

    giving spaces in catalogue file

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,6 +1,6 @@
 print(){
   LSPACE=$(echo $1 | awk '{print length}')
-  SPACES=$9$MSPACE-$LSPACE
+  SPACES=$MSPACE-$LSPACE
   echo -n -e "\e[1m$1\e[0m........"
   echo -e "\n\e[36m#######################$1#######################\e[0m" >>$LOG
 }
