9c98c2b42fc8875bc1446c2dbe72769c9fdef18e
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 16:42:09 +0530

    check function

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -91,7 +91,7 @@ CHECK_MONGO_FROM_APP(){
 CHECK_REDIS_FROM_APP(){
   print "checking DB connections from app"
   sleep 5
-  STAT=$(curl -s localhost:8080/health | jq .redis)
+  STAT=$(curl -s localhost:8080/health | jq .reddis)
   if [ "$STAT" == "true" ]; then
     stat 0
   else
