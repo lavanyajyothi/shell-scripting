9e38d305a0fe8a1624ebf40bac40d82ba9462f85
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 14:03:28 +0530

    giving spaces to put it in order

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,6 +1,11 @@
 print() {
   LSPACE=$(echo $1 | awk '{print length}')
   SPACE=$(($MSPACE-$LSPACE))
+  while [ $SPACE -gt 0 ]; do
+    SPACES="$SPACES$(echo ' ')"
+    $SPACE=$(($SPACE-1))
+  done
+  echo $SPACES | cat -A
  echo -n -e "\e[1m$1${SPACE}\e[0m........"
  echo -e "\n\e[36m#######################$1#######################\e[0m" >>$LOG
 }
