1a3ffb4373ff2930ae369705dd62257352eec16b
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 15:43:32 +0530

    Creating user file

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -22,4 +22,47 @@ stat(){
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
+NodeJS(){
+  print "install NodeJS"
+  yum install nodejs make gcc-c++ -y &>>$LOG
+  stat $?
+
+
+  print "Add Roboshop "
+  id roboshop &>>$LOG
+  if [ $? -eq 0 ]; then
+    echo user roboshop already exist &>>$LOG
+  else
+    useradd roboshop &>>$LOG
+  fi
+  stat $?
+
+
+  print "Download $(COMPONENT_NAME)"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+  stat $?
+
+  print "Remove Old Content"
+  rm -rf /home/roboshop/${COMPONENT}
+  stat $?
+
+  print "Extract ${COMPONENT_NAMET}"
+  unzip -o -d /home/roboshop /tmp/${COMPONENT}.zip &>>$LOG
+  stat $?
+
+  print "Copy Content"
+  mv /home/roboshop/${COMPONENT}-main /home/roboshop/${COMPONENT}
+  stat $?
+
+  print "Install NodeJS Dependencies"
+  cd /home/roboshop/${COMPONENT}
+  npm install --unsafe-perm &>>$LOG
+  stat $?
+
+  print "Fix App Permissions"
+  chown -R roboshop:roboshop /home/roboshop
+  stat $?
+
+}
+
 
---
+++ roboshop/components/user.sh
@@ -0,0 +1,29 @@
+
+source components/common.sh
+
+MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+COMPONENT_NAMET = User
+COMPONENT=user
+NodeJS
+
+
+print "Update DNS records in SystemD config"
+sed -i -e"s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/roboshop/catalogue/systemd.service &>>$LOG
+stat $?
+print "Copy SystemD file"
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+stat $?
+print "start catalogue service"
+systemctl daemon-reload &>>$LOG && systemctl restart catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
+stat $?
+
+sleep 5
+
+print "checking DB connections from app"
+STAT=$(curl -s localhost:8080/health | jq .mongo)
+echo status = $STAT
+if [ $STAT == "true" ]; then
+  stat 0
+else
+  stat 1
+fi
