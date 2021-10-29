022fef93732b856657d2945a476f2aa2a4cf2a96
lavanya <lavanya.bhadri@gmail.com>
2021-10-29 15:46:30 +0530

    calling common nodeJS function

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -4,46 +4,9 @@ source components/common.sh
 
 MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
-
-print "install NodeJS"
-yum install nodejs make gcc-c++ -y &>>$LOG
-stat $?
-
-
-print "Add Roboshop User"
-id roboshop &>>$LOG
-if [ $? -eq 0 ]; then
-  echo user roboshop already exist &>>$LOG
-else
-  useradd roboshop &>>$LOG
-fi
-stat $?
-
-
-print "Download Catalogue"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
-stat $?
-
-print "Remove Old Content"
-rm -rf /home/roboshop/catalogue
-stat $?
-
-print "Extract Catalogue"
-unzip -o -d /home/roboshop /tmp/catalogue.zip &>>$LOG
-stat $?
-
-print "Copy Content"
-mv /home/roboshop/catalogue-main /home/roboshop/catalogue
-stat $?
-
-print "Install NodeJS Dependencies"
-cd /home/roboshop/catalogue
-npm install --unsafe-perm &>>$LOG
-stat $?
-
-print "Fix App Permissions"
-chown -R roboshop:roboshop /home/roboshop
-stat $?
+COMPONENT_NAME=Catalogue
+COMPONENT=catalogue
+NodeJS
 
 print "Update DNS records in SystemD config"
 sed -i -e"s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/roboshop/catalogue/systemd.service &>>$LOG
