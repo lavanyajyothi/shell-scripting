8204b430869d4b3948e9aa69d9a2a53af82671e2
lavanya <lavanya.bhadri@gmail.com>
2021-10-30 05:35:43 +0530

    create instance

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -7,6 +7,8 @@ else
   echo "Instance already exist"
 fi
 
+sleep 5
+
 IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null)
 sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z08537272MHAQMEL5KTIG --change-batch file:///tmp/record.json | jq
