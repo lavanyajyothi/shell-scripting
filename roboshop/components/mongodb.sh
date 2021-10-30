3d703f9ea19184f97e305cfc4db8f64a2ad1a3f2
lavanya <lavanya.bhadri@gmail.com>
2021-10-30 12:49:16 +0530

    make file

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -2,7 +2,10 @@
 
 source components/common.sh
 
-MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+MSPACE=$(cat $0 | grep print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+
+COMPONENT_NAME=MongoDB
+COMPONENT=mongodb
 
 print "Download Repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
@@ -25,17 +28,13 @@ print "enable MongoDB"
 systemctl enable mongod &>>$LOG
 stat $?
 
-print "Download Schema"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
-stat $?
-
-print "Extract Schema"
-unzip -o -d /tmp /tmp/mongodb.zip &>>$LOG
-stat $?
+DOWNLOAD "/tmp"
 
 print "Load Schema"
 cd /tmp/mongodb-main
-mongo < catalogue.js &>>$LOG
-mongo < users.js &>>$LOG
+for db in catalogue users ; do
+  mongo < $db.js &>>$LOG
+done
+
 stat $?
 
