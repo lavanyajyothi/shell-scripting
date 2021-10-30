b61852099d60f262dea4781e2062c41fda69aab2
lavanya <lavanya.bhadri@gmail.com>
2021-10-30 12:43:20 +0530

    make file

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,13 +7,6 @@ MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }'
 print "Installing Nginx"
 yum install nginx -y &>>$LOG
 stat $?
-print "Enabling Nginx"
-systemctl enable nginx &>>$LOG
-stat $?
-
-print "starting Nginx"
-systemctl start nginx &>>$LOG
-stat $?
 
 print "download Html pages"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"&>>$LOG
@@ -34,6 +27,10 @@ stat $?
 print "copy nginx roboshop config file"
 cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
+print "Update Nginx Config file"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e 'payment/ s/localhost/payment.roboshop.interal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG
+stat $?
+
 
 print "enabling nginx"
 systemctl enable nginx &>>LOG
