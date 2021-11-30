0e0bcb973da5801f871d3636ce7ad27a5ef2294e
lavanya <lavanya.bhadri>
2021-11-30 23:07:28 +0530

    create instances

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -2,7 +2,7 @@
 CREATE(){
   COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
 if [ $COUNT -eq 0 ]; then
-  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --security-group-ids sg-0370951ca6dfcb3e7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+  aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --security-group-ids sg-0370951ca6dfcb3e7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
 else
   echo -e "\e[1;33m$1 Instance already exists\e[0m"
   return
@@ -25,11 +25,3 @@ if [ "$1" == "all"]; then
 else
   CREATE $1
 fi
-
-    ;;
-  esac
-
-
-
-
-}
