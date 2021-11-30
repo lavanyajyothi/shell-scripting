161ad30eb15e74344249051b6645cab4b4f5af51
lavanya <lavanya.bhadri>
2021-11-30 23:12:33 +0530

    create instances

--- roboshop/create-spot-instance.sh
+++ roboshop/create-spot-instance.sh
@@ -4,7 +4,7 @@ CREATE() {
   COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
   if [ $COUNT -eq 0 ]; then
-    aws ec2 run-instances --launch-template LaunchTemplateId=,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+    aws ec2 run-instances --launch-template LaunchTemplateId=lt-02de5b54b6b56e7f9,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
     return
@@ -15,7 +15,7 @@ CREATE() {
   IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
   ## xargs is used to remove the double  quotes
   sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z0577679A6W027W86RBE --change-batch file:///tmp/record.json | jq  &>/dev/null
+  aws route53 change-resource-record-sets --hosted-zone-id Z08537272MHAQMEL5KTIG --change-batch file:///tmp/record.json | jq  &>/dev/null
 }
 
 if [ "$1" == "all" ]; then
@@ -27,11 +27,3 @@ if [ "$1" == "all" ]; then
 else
   CREATE $1
 fi
-
-
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
